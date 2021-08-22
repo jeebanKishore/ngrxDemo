db684f46d33e97bc61e2643334c418baf37f0759
Jeeban Mohanty <jeemohan@publicisgroupe.net>
2021-08-22 23:53:16 +0530

    Added FC

---
+++ src/app/reducers/tutorial.reducer.ts
@@ -0,0 +1,28 @@
+import { Action } from '@ngrx/store'
+import { Tutorial } from './../models/tutorial.model'
+import * as TutorialActions from './../actions/tutorial.actions'
+import { state } from '@angular/animations';
+
+const initialState: Tutorial = {
+    name: 'Initial Tutorial',
+    url: 'http://google.com'
+}
+
+export function _reducer(state: Tutorial[] = [initialState], action: TutorialActions.Actions) {
+  console.log(state, action);
+    switch(action.type) {
+        case TutorialActions.ADD_TUTORIAL:
+            return [...state, action.payload];
+
+        case TutorialActions.REMOVE_TUTORIAL:
+          state = [...state];
+            state.splice(action.payload, 1)
+            return state;
+        default:
+            return state;
+    }
+}
+
+export function reducer(state: any, action:any){
+  return _reducer(state, action);
+}
